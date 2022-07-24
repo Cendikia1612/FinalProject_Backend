35c28a0d7277fde7936d755771ac2e2aeded249d
Cendikia <Halizah1612@gmail.com>
2022-07-25 00:52:16 +0700

    update seeders user

--- database/seeders/20220724062210-UserSeeders.js
+++ database/seeders/20220724062210-UserSeeders.js
@@ -7,30 +7,30 @@ module.exports = {
     const password = bcrypt.hashSync(pass, 10);
     const timestamp = new Date();
 
-    const sellerA = [
+    const seller1 = [
       {
         role: "SELLER",
-        name: "SELLER A",
-        email: "cendikia1612@gmail.com",
+        name: "SELLER 1",
+        email: "seller1@gmail.com",
         password,
-        city: "Bandung",
+        city: "Semarang",
         address: "Jl. merdeka",
-        phone: "081215842130",
+        phone: "081879742130",
         image:
           "https://res.cloudinary.com/dy0o6o4cl/image/upload/v1658385592/jthb9fsmxl9lgrvhftxr.jpg",
         createdAt: timestamp,
         updatedAt: timestamp,
       },
     ];
-    const sellerB = [
+    const seller2 = [
       {
         role: "SELLER",
-        name: "SELLER B",
-        email: "adam@gmail.com",
+        name: "SELLER 2",
+        email: "seller2@gmail.com",
         password,
         city: "Malang",
-        address: "Jl.Mawar ",
-        phone: "081576453423",
+        address: "Jl.kumbang ",
+        phone: "081576453489",
         image:
           "https://res.cloudinary.com/dy0o6o4cl/image/upload/v1658645590/ow6vub82nzqah2d524hw.jpg",
         createdAt: timestamp,
@@ -39,14 +39,14 @@ module.exports = {
     ];
 
 
-    const buyerA = [
+    const buyer1 = [
       {
         role: "BUYER",
-        name: "BUYER A",
+        name: "BUYER 1",
         email: "buyer1@gmail.com",
         password,
         city: "Bekasi",
-        address: "Jl. Melati",
+        address: "Jl. cempaka",
         phone: "62342342321",
         image:
           "https://res.cloudinary.com/dy0o6o4cl/image/upload/v1658646163/oen1hez3y0mkdw7x5erz.jpg",
@@ -55,14 +55,14 @@ module.exports = {
       },
     ];
 
-    const buyerB = [
+    const buyer2 = [
       {
         role: "BUYER",
-        name: "BUYER B",
+        name: "BUYER c",
         email: "buyer2@gmail.com",
         password,
         city: "Surabaya",
-        address: "Jl. Soekarno",
+        address: "Jl. ahmad yamin",
         phone: "62394999212",
         image:
           "https://res.cloudinary.com/dy0o6o4cl/image/upload/v1658646464/qxqgo6httaxjhb3bepp0.jpg",
@@ -71,10 +71,10 @@ module.exports = {
       },
     ];
 
-    await queryInterface.bulkInsert("Users", sellerA, {});
-    await queryInterface.bulkInsert("Users", sellerB, {});
-    await queryInterface.bulkInsert("Users", buyerA, {});
-    await queryInterface.bulkInsert("Users", buyerB, {});
+    await queryInterface.bulkInsert("Users", seller1, {});
+    await queryInterface.bulkInsert("Users", seller2, {});
+    await queryInterface.bulkInsert("Users", buyer1, {});
+    await queryInterface.bulkInsert("Users", buyer2, {});
   },
 
   async down (queryInterface, Sequelize) {
